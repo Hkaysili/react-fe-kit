56df50808cae839e76cd9f351f545b0b2088da88
Harun Veli KAYSILI <harun.kaysili@dygdigital.com>
2022-07-17 14:48:49 +0300

    readme update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-       <img src={logo} alt="logo" />
+       <img src={logo} alt="logo" /> 
     </div>
-  );
+  ); 
 }
 
 export default App;
