29b75845e9a7be51fdaf805eac3a529b35e041b3
Harun Veli KAYSILI <harun.kaysili@dygdigital.com>
2022-07-17 15:10:31 +0300

    Ortam Degiskenleri uzerine denemeler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import logo from './logo.svg'; 
 
 function App() {
+  useEffect(()=>{
+    if(process.env.NODE_ENV === 'production'){
+      // analytic'i bilgilendir..
+    }
+  }, [])
   return (
     <div className="App">
-       <img src={logo} alt="logo" /> 
+       <h3>{process.env.NODE_ENV}</h3>
+       <span>My Api URL : {process.env.REACT_APP_API_URL}</span>
+       {process.env.NODE_ENV === 'production' && (
+         <>
+          <img src={logo} alt="logo" /> 
+         </>
+       )}
     </div>
   ); 
 }
